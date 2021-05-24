27291f0b5fa3e3820be57aabf36e119de0db9e87
mazhar.rayhan <mazhar.rayhan@dimikit.net>
2021-05-24 16:09:23 +0600

    the options are rendered correctly and styled the components simply

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import {questions} from './api/questions'
 
 
 function App() {
+  const options = [];
+
+  questions.forEach((question)=>{
+    const option = [...question.incorrect_answers,question.correct_answer];
+    option.sort(()=>Math.random()-0.5);
+    options.push(option);
+  })
+
   function decodeString(str){
     const textArea = document.createElement('textarea')
     textArea.innerHTML = str
@@ -13,11 +21,13 @@ function App() {
   return (
     <div>
         {questions.map((q,i)=>
-          <div key={i}>
-            <h3>{decodeString(q.question)}</h3>
-            {q.incorrect_answers.map((ia,j)=>
-              <p key={j}>{ia}</p>
-            )}
+          <div key={i} className="App">
+            <h3 className="title">{decodeString(q.question)}</h3>
+            <div>
+              {options[i].map((ia,j)=>
+                <span key={j} className="option" onClick={()=>alert(ia)}>{ia}</span>
+              )}
+            </div>
           </div>
         ) }
     </div>
